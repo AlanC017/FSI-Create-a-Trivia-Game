cb8fd77e167aea2e9e7aeb8107ebf3ccaff44419
Alan Cobos <imacretina5k@Alans-iMac.local>
2022-12-28 20:40:35 -0800

    All

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your Code Here
+let userName = window.prompt("Welcome! What is your name?")
+console.log("userName", userName)
+let userScore = 0
+
+//prompt user with each question:
+for (let i = 0; i < questions.length; i++){
+    let question = questions [i]
+    let userAnswer = window.prompt(question.text)
+    console.log(userAnswer)
+    if (userAnswer.toUpperCase() === question.correctAnswer) {
+        console.log(`Question ${i+1} is correct`)
+        userScore += 10
+        }   else {
+            window.alert(`Question ${i+1} is incorrect`)
+        }
+        window.alert("Your score is " +userScore)
+}
+
+//show user score
+window.alert("Congrats! Your final score is " +userScore)
